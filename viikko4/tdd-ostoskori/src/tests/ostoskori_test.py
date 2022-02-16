0cb5e855c1db54e8bc0d947fab12af307a10aa7b
TuuPu <puontit@gmail.com>
2022-02-16 16:35:44 +0200

    Tehtävä5 osa12 tehty. Lisätty kaksi kertaa maito ostoskoriin, jonka jälkeen otettu ostos-muuttujaan ostoslistan ensimmäinen alkio, tarkistettu, että ostos.tuotteen_nimi() on sama kuin maito.nimi() ja ostos.lukumaara() on 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -79,3 +79,12 @@ class TestOstoskori(unittest.TestCase):
         ostokset =  self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+#12. Kahden saman tuotteen lisäämisen jälkeen ostoskori sisältää ostoksen
+# jolla sama nimi kuin tuotteella ja lukumäärä 2
+    def test_kahden_saman_tuotteen_lisaaminen_sisaltaa_ostoksen_jolla_sama_nimi_kuin_tuotteella_ja_lkm2(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), maito.nimi())
+        self.assertEqual(ostos.lukumaara(), 2)
