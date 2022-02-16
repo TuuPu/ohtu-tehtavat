ac193c34d9ae93a5d07f23713b3adb352d1be7d5
TuuPu <puontit@gmail.com>
2022-02-16 11:57:53 +0200

    Tehtävät 1 ja 2 puuhasteltu

---
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import Mock, ANY
+from kassapaate import Kassapaate, HINTA
+from maksukortti import Maksukortti
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassa = Kassapaate()
+
+    def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_called_with(HINTA)
+
+    def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+        
+        self.kassa.osta_lounas(maksukortti_mock)
+
+        maksukortti_mock.osta.assert_not_called()
+
+    def test_kassapaate_lataa_kutsu_lisaa_maksukortille_ladattavan_rahamaaran(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock, 4)
+
+        maksukortti_mock.lataa.assert_called_with(4)
+
+    def test_kassapaate_ei_lataa_rahaa_maksukortille_jos_summa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+
+        self.kassa.lataa(maksukortti_mock, -13)
+
+        maksukortti_mock.lataa.assert_not_called()
+
+#Kassapäätteen metodin lataa kutsu lisää maksukortille ladattavan rahamäärän
+#käyttäen kortin metodia lataa jos ladattava summa on positiivinen
+
+#Kassapäätteen metodin lataa kutsu ei tee maksukortille mitään jos ladattava summa on negatiivinen
