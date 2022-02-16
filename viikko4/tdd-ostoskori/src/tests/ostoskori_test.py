ca0b260fc0ce4561aab3bf4ba7304af4ac4eaac1
TuuPu <puontit@gmail.com>
2022-02-16 15:12:28 +0200

    Tehtävä5 osa4 testi luotu. Testissä luotiin kaksi tuotetta: Maito ja Kaali. Jotka lisättiin koriin ja sen jälkeen todettiin, että korissa on kaksi tavaraa aivan oikein.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+#4. Kahden eri tuotteen lisäämisen jälkeen ostoskorissa on 2 tavaraa
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        kaali = Tuote("Kaali", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kaali)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
