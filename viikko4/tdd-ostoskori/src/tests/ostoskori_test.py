c0571ed1d1ac46c1fccd3da0b1975ad1757f29ba
TuuPu <puontit@gmail.com>
2022-02-16 15:23:03 +0200

    Tehtävä5 osa6 tehty. Testissä lisätty kaksi kertaa maito ja tarkistettu, että tavaroita korissa summa on 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -33,3 +33,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(kaali)
         self.assertEqual(self.kori.hinta(), 8)
+#6. Kahden saman tuotteen lisäämisen jälkeen ostoskorissa on 2 tavaraa
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
