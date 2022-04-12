93c3eb92893532caccfc77f0577765295204d1f7
TuuPu <puontit@gmail.com>
2022-04-12 13:43:39 +0300

    Viikko6 tehtävä 4 tehty

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,18 @@
+from matchers import *
+
+class QueryBuilder:
+    def __init__(self, matcher = All()):
+        self._matcher = matcher
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+
+    def hasAtLeast(self, goals, attr):
+        return QueryBuilder(And(self._matcher, HasAtLeast(goals, attr)))
+
+    def hasFewerThan(self, goals, attr):
+        return QueryBuilder(And(self._matcher, HasFewerThan(goals, attr)))
+
+
+    def build(self):
+        return self._matcher
